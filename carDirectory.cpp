fc4b0b52d58ba78d648c71819b2d51e0d5e6d87c
niffy <niffyjay2001@gmail.com>
2023-12-20 08:38:39 +0200

    i added the code description to the beginning of the code

--- carDirectory.cpp
+++ carDirectory.cpp
@@ -1,6 +1,12 @@
 //
 //  main.cpp
-//  homework1
+//  This code works for a car directory. 
+//It adds car details(owner's name, surname, car id, car model,regsitration date and service date of the car). 
+//It can be used to delete cars from directory. 
+//It list the available cars in the directory. 
+//It can be used to search for cars in the directory(search according to car id, search according to owner's name). 
+///It can update the car information already saved.
+//It can sort the cars in the directory (according to car id, according to owner's name)
 //
 //  Created by Machine on 1/9/23.
 //
